26fc444c4597b6eb23a5db9048603e3ed451cd3a
Brian Grant and Sami Al-Jamal <sami@>
2016-06-20 14:21:27 -0700

    create sound object and instances for highHat and bassDrum.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,27 @@
 $(document).ready(function() {
 
+  var highHat = new Sound("audio/HHOPEN2.wav");
+  $("#high-hat").click(function(event) {
+    highHat.stop();
+    highHat.play();
+  });
+  var bassDrum = new Sound("audio/BDRUM13.wav");
+  $("#bass-drum").click(function(event) {
+    bassDrum.stop();
+    bassDrum.play();
+  });
+
 });
+
+
+function Sound(src) {
+    this.sound = document.createElement("audio");
+    this.sound.src = src;
+    this.play = function(){
+        this.sound.play();
+    }
+    this.stop = function(){
+        this.sound.pause();
+        this.sound.currentTime = 0;
+    }
+}
