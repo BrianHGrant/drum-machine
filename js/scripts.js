6d748fa72d9b1a94f2566cbb992f5fae86b85fcf
Adam Luchini <adam@>
2016-06-21 13:52:29 -0700

    snare, bongo, cymbal crash

--- js/scripts.js
+++ js/scripts.js
@@ -10,6 +10,21 @@ $(document).ready(function() {
     bassDrum.stop();
     bassDrum.play();
   });
+  snareDrum = new Sound("audio/SNARE12.wav");
+  $("#snare-drum").click(function(event) {
+    snareDrum.stop();
+    snareDrum.play();
+  });
+  bongoDrum = new Sound("audio/BONGO1.wav");
+  $("#bongo-drum").click(function(event) {
+    bongoDrum.stop();
+    bongoDrum.play();
+  });
+  cymbalCrash = new Sound("audio/crash.mp3");
+  $("#cymbal-crash").click(function(event) {
+    cymbalCrash.stop();
+    cymbalCrash.play();
+  });
 
   $("body").keydown(function(event){
     if (event.keyCode === 81){
@@ -21,13 +36,27 @@ $(document).ready(function() {
       bassDrum.play();
     }
 
-    if (event.keyCode === 82){
-      highHat.loopsound();
+    if (event.keyCode === 69){
+      snareDrum.stop();
+      snareDrum.play();
     }
 
-    if (event.keyCode === 69){
-      bassDrum.loopsound();
+    if (event.keyCode === 82){
+      bongoDrum.stop();
+      bongoDrum.play();
     }
+    if (event.keyCode === 84){
+      cymbalCrash.stop();
+      cymbalCrash.play();
+    }
+    //
+    // if (event.keyCode === 82){
+    //   highHat.loopsound();
+    // }
+    //
+    // if (event.keyCode === 69){
+    //   bassDrum.loopsound();
+    // }
 
   });
 
@@ -46,6 +75,15 @@ $(document).ready(function() {
       else if ($(this).val()==="highHat") {
         firstPosition.push(highHat);
       }
+      else if ($(this).val()==="snareDrum") {
+        firstPosition.push(snareDrum);
+      }
+      else if ($(this).val()==="bongoDrum"){
+        firstPosition.push(bongoDrum);
+      }
+      else if ($(this).val()==="cymbalCrash"){
+        firstPosition.push(cymbalCrash);
+      }
       currentLoop.addSound(firstPosition);
     });
 
@@ -56,6 +94,15 @@ $(document).ready(function() {
         }
       else if ($(this).val()==="highHat") {
         secondPosition.push(highHat);
+      }
+      else if ($(this).val()==="snareDrum") {
+        secondPosition.push(snareDrum);
+      }
+      else if ($(this).val()==="bongoDrum"){
+        secondPosition.push(bongoDrum);
+      }
+      else if ($(this).val()==="cymbalCrash"){
+        secondPosition.push(cymbalCrash);
       }
       currentLoop.addSound(secondPosition);
     });
@@ -68,6 +115,15 @@ $(document).ready(function() {
       else if ($(this).val()==="highHat") {
         thirdPosition.push(highHat);
       }
+      else if ($(this).val()==="snareDrum") {
+        thirdPosition.push(snareDrum);
+      }
+      else if ($(this).val()==="bongoDrum"){
+        thirdPosition.push(bongoDrum);
+      }
+      else if ($(this).val()==="cymbalCrash"){
+        thirdPosition.push(cymbalCrash);
+      }
       currentLoop.addSound(thirdPosition);
     });
 
@@ -78,6 +134,15 @@ $(document).ready(function() {
         }
       else if ($(this).val()==="highHat") {
         fourthPosition.push(highHat);
+      }
+      else if ($(this).val()==="snareDrum") {
+        fourthPosition.push(snareDrum);
+      }
+      else if ($(this).val()==="bongoDrum"){
+        fourthPosition.push(bongoDrum);
+      }
+      else if ($(this).val()==="cymbalCrash"){
+        fourthPosition.push(cymbalCrash);
       }
       currentLoop.addSound(fourthPosition);
     });
@@ -112,36 +177,72 @@ $(document).ready(function() {
 function playLoop() {
   for (var i=0;i<currentLoop.sounds[0].length; i++) {
     if(currentLoop.sounds[0][i] === highHat){
-      setTimeout(playSound2, 0);
+      setTimeout(highHatSound, 0);
     }
     if (currentLoop.sounds[0][i] === bassDrum) {
-      setTimeout(playSound, 0);
+      setTimeout(bassSound, 0);
+    }
+    if (currentLoop.sounds[0][i] === snareDrum){
+      setTimeout(snareSound, 0);
+    }
+    if (currentLoop.sounds[0][i] === bongoDrum){
+      setTimeout(bongoSound, 0);
+    }
+    if (currentLoop.sounds[0][i] === cymbalCrash){
+      setTimeout(cymbalSound, 0);
     }
   }
 
   for (var i=0;i<currentLoop.sounds[1].length; i++) {
     if(currentLoop.sounds[1][i] === highHat){
-      setTimeout(playSound2, 1000);
+      setTimeout(highHatSound, 1000);
     }
     if (currentLoop.sounds[1][i] === bassDrum) {
-      setTimeout(playSound, 1000);
+      setTimeout(bassSound, 1000);
+    }
+    if (currentLoop.sounds[1][i] === snareDrum){
+      setTimeout(snareSound, 1000);
+    }
+    if (currentLoop.sounds[1][i] === bongoDrum){
+      setTimeout(bongoSound, 1000);
+    }
+    if (currentLoop.sounds[1][i] === cymbalCrash){
+      setTimeout(cymbalSound, 1000);
     }
   }
 
   for (var i=0;i<currentLoop.sounds[2].length; i++) {
     if(currentLoop.sounds[2][i] === highHat){
-      setTimeout(playSound2, 2000);
+      setTimeout(highHatSound, 2000);
     }
     if (currentLoop.sounds[2][i] === bassDrum) {
-      setTimeout(playSound, 2000);
+      setTimeout(bassSound, 2000);
+    }
+    if (currentLoop.sounds[2][i] === snareDrum){
+      setTimeout(snareSound, 2000);
+    }
+    if (currentLoop.sounds[2][i] === bongoDrum){
+      setTimeout(bongoSound, 2000);
+    }
+    if (currentLoop.sounds[2][i] === cymbalCrash){
+      setTimeout(cymbalSound, 2000);
     }
   }
   for (var i=0;i<currentLoop.sounds[3].length; i++) {
     if(currentLoop.sounds[3][i] === highHat){
-      setTimeout(playSound2, 3000);
+      setTimeout(highHatSound, 3000);
     }
     if (currentLoop.sounds[3][i] === bassDrum) {
-      setTimeout(playSound, 3000);
+      setTimeout(bassSound, 3000);
+    }
+    if (currentLoop.sounds[3][i] === snareDrum){
+      setTimeout(snareSound, 3000);
+    }
+    if (currentLoop.sounds[3][i] === bongoDrum){
+      setTimeout(bongoSound, 3000);
+    }
+    if (currentLoop.sounds[3][i] === cymbalCrash){
+      setTimeout(cymbalSound, 3000);
     }
   }
   // setTimeout(playSound, 0);
@@ -150,12 +251,24 @@ function playLoop() {
   // setTimeout(playSound2, 3000);
 }
 
-function playSound() {
+function bassSound() {
   bassDrum.play();
 }
 
-function playSound2() {
+function highHatSound() {
   highHat.play();
+}
+
+function snareSound() {
+  snareDrum.play();
+}
+
+function bongoSound() {
+  bongoDrum.play();
+}
+
+function cymbalSound() {
+  cymbalCrash.play();
 }
 
 function Sound(src) {
