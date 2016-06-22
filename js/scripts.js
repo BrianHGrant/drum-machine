c2d24dd6ad0c57c19d5bc9b35bb18ed85119b374
BrianHGrant <bhgrant8@gmail.com>
2016-06-22 16:43:38 -0700

    remove unused code and add container and title

--- js/scripts.js
+++ js/scripts.js
@@ -17,71 +17,46 @@ for(var i=1; i<6; i++){
 
   var highHat = new Sound("audio/HHOPEN2.wav");
   $(".instrument2").click(function(event) {
-    highHat.stop();
     highHat.play();
   });
   var bassDrum = new Sound("audio/BDRUM13.wav");
   $(".instrument3").click(function(event) {
-    bassDrum.stop();
     bassDrum.play();
   });
   var snareDrum = new Sound("audio/SNARE12.wav");
   $(".instrument1").click(function(event) {
-    snareDrum.stop();
     snareDrum.play();
   });
   var bongoDrum = new Sound("audio/BONGO1.wav");
   $(".instrument4").click(function(event) {
-    bongoDrum.stop();
     bongoDrum.play();
   });
   var cymbalCrash = new Sound("audio/crash.mp3");
   $(".instrument5").click(function(event) {
-    cymbalCrash.stop();
     cymbalCrash.play();
   });
 
   $("body").keydown(function(event){
     if (event.keyCode === 81){
-      highHat.stop();
       highHat.play();
-      $("#high-hat").css("background-color", "red");
-    }
-    if (event.keyCode === 87){
-      bassDrum.stop();
-      bassDrum.play();
-    }
-
-    if (event.keyCode === 69){
-      snareDrum.stop();
-      snareDrum.play();
-    }
-
-    if (event.keyCode === 82){
-      bongoDrum.stop();
-      bongoDrum.play();
-    }
-    if (event.keyCode === 84){
-      cymbalCrash.stop();
-      cymbalCrash.play();
-    }
-  });
-  $("body").keydown(function(event){
-    if (event.keyCode === 81){
       $("#instrument1").css("background-color", "grey");
     }
     if (event.keyCode === 87){
+      bassDrum.play();
       $("#instrument2").css("background-color", "grey");
     }
 
     if (event.keyCode === 69){
+      snareDrum.play();
       $("#instrument3").css("background-color", "grey");
     }
 
     if (event.keyCode === 82){
+      bongoDrum.play();
       $("#instrument4").css("background-color", "grey");
     }
     if (event.keyCode === 84){
+      cymbalCrash.play();
       $("#instrument5").css("background-color", "grey");
     }
   });
@@ -136,12 +111,8 @@ for(var i=1; i<6; i++){
     else{
       var tempo = 60000/ bpm;
       var loopTempo = tempo * 8;
-
-
       playLoop(currentLoop, highHat, bassDrum, snareDrum, bongoDrum, cymbalCrash, tempo);
       var playInterval = setInterval(playLoop, loopTempo, currentLoop, highHat, bassDrum, snareDrum, bongoDrum, cymbalCrash, tempo);
-
-
     }
     $("#stop-loop-btn").click(function(event){
       clearInterval(playInterval);
@@ -150,9 +121,7 @@ for(var i=1; i<6; i++){
       $("#loop-btn").show();
       $("#stop-loop-btn").hide();
     });
-
   });
-
   $("#clear-checked").click(function(evet){
     for (i=0; i<8;i++){
       $('.checbox' + i + ':checked').each(function () {
@@ -181,7 +150,6 @@ function Sound(src) {
 
 Sound.prototype.play = function() {
   this.sound.currentTime = 0;
-  this.sound.durationTime = 250;
   this.sound.play();
 }
 
