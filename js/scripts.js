57ad7632cd11972ddb7f70808bf5faf9f17226af
BrianHGrant <bhgrant8@gmail.com>
2016-06-23 08:26:59 -0700

    add display loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,38 @@
 $(document).ready(function() {
 
-for(var i=1; i<6; i++){
-  $(".instrument"+ i).append(
-       '<label><input type="checkbox" class ="checbox0" name="instrument' + i +'"' +'value="instrument' + i
-      +'"><span></span></label>'
-      +'<label><input type="checkbox" class ="checbox1" name="instrument' + i +'"' +'value="instrument' + i +'"><span></span></label>'
-      +'<label><input type="checkbox" class ="checbox2" name="instrument' + i +'"' +'value="instrument' + i +'"><span></span></label>'
-      +'<label><input type="checkbox" class ="checbox3" name="instrument' + i +'"' +'value="instrument' + i +'"><span></span></label>'
-      +'<label><input type="checkbox" class ="checbox4" name="instrument' + i +'"' +'value="instrument' + i +'"><span></span></label>'
-      +'<label><input type="checkbox" class ="checbox5" name="instrument' + i +'"' +'value="instrument' + i +'"><span></span></label>'
-      +'<label><input type="checkbox" class ="checbox6" name="instrument' + i +'"' +'value="instrument' + i +'"><span></span></label>'
-      +'<label><input type="checkbox" class ="checbox7" name="instrument' + i +'"' +'value="instrument' + i +'"><span></span></label>'
+  for(var i=1; i<6; i++){
+    $(".instrument"+ i).append(
+         '<label><input type="checkbox" class ="checbox0" name="instrument' + i +'"' +'value="instrument' + i
+        +'"><span></span></label>'
+        +'<label><input type="checkbox" class ="checbox1" name="instrument' + i +'"' +'value="instrument' + i +'"><span></span></label>'
+        +'<label><input type="checkbox" class ="checbox2" name="instrument' + i +'"' +'value="instrument' + i +'"><span></span></label>'
+        +'<label><input type="checkbox" class ="checbox3" name="instrument' + i +'"' +'value="instrument' + i +'"><span></span></label>'
+        +'<label><input type="checkbox" class ="checbox4" name="instrument' + i +'"' +'value="instrument' + i +'"><span></span></label>'
+        +'<label><input type="checkbox" class ="checbox5" name="instrument' + i +'"' +'value="instrument' + i +'"><span></span></label>'
+        +'<label><input type="checkbox" class ="checbox6" name="instrument' + i +'"' +'value="instrument' + i +'"><span></span></label>'
+        +'<label><input type="checkbox" class ="checbox7" name="instrument' + i +'"' +'value="instrument' + i +'"><span></span></label>'
 
-  );
-}
+    );
+  }
 
   var highHat = new Sound("audio/HHOPEN2.wav");
+  var bassDrum = new Sound("audio/BDRUM13.wav");
+  var snareDrum = new Sound("audio/SNARE12.wav");
+  var bongoDrum = new Sound("audio/BONGO1.wav");
+  var cymbalCrash = new Sound("audio/crash.mp3");
+
   $(".instrument2").click(function(event) {
     highHat.play();
   });
-  var bassDrum = new Sound("audio/BDRUM13.wav");
   $(".instrument3").click(function(event) {
     bassDrum.play();
   });
-  var snareDrum = new Sound("audio/SNARE12.wav");
   $(".instrument1").click(function(event) {
     snareDrum.play();
   });
-  var bongoDrum = new Sound("audio/BONGO1.wav");
   $(".instrument4").click(function(event) {
     bongoDrum.play();
   });
-  var cymbalCrash = new Sound("audio/crash.mp3");
   $(".instrument5").click(function(event) {
     cymbalCrash.play();
   });
@@ -39,45 +40,45 @@ for(var i=1; i<6; i++){
   $("body").keydown(function(event){
     if (event.keyCode === 81){
       highHat.play();
-      $("#instrument1").css("background-color", "grey");
+      $("#instrument1").addClass("selected");
     }
     if (event.keyCode === 87){
       bassDrum.play();
-      $("#instrument2").css("background-color", "grey");
+      $("#instrument2").addClass("selected");
     }
 
     if (event.keyCode === 69){
       snareDrum.play();
-      $("#instrument3").css("background-color", "grey");
+      $("#instrument3").addClass("selected");
     }
 
     if (event.keyCode === 82){
       bongoDrum.play();
-      $("#instrument4").css("background-color", "grey");
+      $("#instrument4").addClass("selected");
     }
     if (event.keyCode === 84){
       cymbalCrash.play();
-      $("#instrument5").css("background-color", "grey");
+      $("#instrument5").addClass("selected");
     }
   });
 
   $("body").keyup(function(event){
     if (event.keyCode === 81){
-      $("#instrument1").css("background-color", "");
+      $("#instrument1").removeClass("selected");
     }
     if (event.keyCode === 87){
-      $("#instrument2").css("background-color", "");
+      $("#instrument2").removeClass("selected");
     }
 
     if (event.keyCode === 69){
-      $("#instrument3").css("background-color", "");
+      $("#instrument3").removeClass("selected");
     }
 
     if (event.keyCode === 82){
-      $("#instrument4").css("background-color", "");
+      $("#instrument4").removeClass("selected");
     }
     if (event.keyCode === 84){
-      $("#instrument5").css("background-color", "");
+      $("#instrument5").removeClass("selected");
     }
   });
 
@@ -106,7 +107,7 @@ for(var i=1; i<6; i++){
       });
     }
     if(bpm<60 || bpm>6000 || !bpm){
-      $(".tooSmall").text("bpm has to be in range of 60-6000");
+      $(".tooSmall").text("BPM has to be in range of 60-6000");
     }
 
     else{
@@ -115,6 +116,7 @@ for(var i=1; i<6; i++){
       playLoop(currentLoop, highHat, bassDrum, snareDrum, bongoDrum, cymbalCrash, tempo);
       var playInterval = setInterval(playLoop, loopTempo, currentLoop, highHat, bassDrum, snareDrum, bongoDrum, cymbalCrash, tempo);
     }
+
     $("#stop-loop-btn").click(function(event){
       clearInterval(playInterval);
       $(".tooSmall").text("");
@@ -123,6 +125,7 @@ for(var i=1; i<6; i++){
       $("#stop-loop-btn").hide();
     });
   });
+
   $("#clear-checked").click(function(evet){
     for (i=0; i<8;i++){
       $('.checbox' + i + ':checked').each(function () {
@@ -134,10 +137,16 @@ for(var i=1; i<6; i++){
 
 function playLoop(currentLoop, highHat, bassDrum, snareDrum, bongoDrum, cymbalCrash, tempo) {
   for(var j=0; j<8; j++){
+    setTimeout(colorBack, j*tempo, j, "grey");
+    setTimeout(colorBack, j*tempo+tempo, j, "blue");
     for (var i=0;i<currentLoop.sounds[j].length; i++) {
-      setTimeout(playSound, j*tempo, currentLoop.sounds[j][i])
+      setTimeout(playSound, j*tempo, currentLoop.sounds[j][i]);
     }
   }
+}
+
+function colorBack(j, color) {
+  $(".checbox" + j +":input[type=checkbox]:checked ~ span").css("background-color", color);
 }
 
 function playSound(sound) {
